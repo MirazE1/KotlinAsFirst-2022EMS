194db628c9e2a1620cc9f7436a71c769640e04c0
MirazE1 <mirazelyubaev@gmail.com>
2022-10-08 13:44:18 +0300

    isCoPrime

--- test/lesson3/task1/Tests.kt
+++ test/lesson3/task1/Tests.kt
@@ -151,7 +151,7 @@ class Tests {
     fun isCoPrime() {
         assertTrue(isCoPrime(25, 49))
         assertFalse(isCoPrime(6, 8))
-        assertTrue(isCoPrime(1, 1))
+        assertTrue(isCoPrime(17, 97))
         assertFalse(isCoPrime(37, 111))
         assertTrue(isCoPrime(1234567890, 908765431))
         assertTrue(isCoPrime(2109876543, 1234567891))
