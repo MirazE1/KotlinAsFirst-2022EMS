68f67024cb2532e1ae116402c1a2fb2e2fb5f257
MirazE1 <mirazelyubaev@gmail.com>
2022-11-12 19:00:13 +0300

    map

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -303,7 +303,7 @@ fun propagateHandshakes(friends: Map<String, Set<String>>): Map<String, Set<Stri
  */
 fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> {
     for (i in list)
-        if (abs(number - i) in list.filter { it != i })
+        if (abs(number - i) in list && list.indexOf(i) != list.indexOf(abs(number - i)))
             return Pair(list.indexOf(minOf(i, abs(number - i))), list.indexOf(maxOf(i, abs(number - i))))
     return Pair(-1, -1)
 }
